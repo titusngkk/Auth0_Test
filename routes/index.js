76ec4f26b3fc05f2a264c03a25d1d1a7774c6a82
betternut <debjeanlee.is@gmail.com>
2022-08-04 11:27:54 +0800

    hello

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,32 @@
-var express = require("express")
-var router = express.Router()
-const { requiresAuth } = require('express-openid-connect')
+var express = require("express");
+var router = express.Router();
+const { requiresAuth } = require("express-openid-connect");
 
 router.get("/", (req, res) => {
-    console.log(req.oidc.isAuthenticated());
-    res.render("index", { 
-    title: "iGoInvest", 
+  console.log(req.oidc.isAuthenticated());
+  res.render("index", {
+    title: "iGoInvest",
     isAuthenticated: req.oidc.isAuthenticated(),
     user: req.oidc.user,
- });
+  });
 });
 
 router.get("/dashboard", requiresAuth(), (req, res) => {
-    res.render("dashboard", { 
-    title: "Dashboard", 
+  if (req.oidc.isAuthenticated()) {
+    console.log("auth");
+    // find out how to pull details from auth0
+    // make post request to backend to store user data - check axios
+    // axios.post(`${process.env.API}/users/add`, {
+    //     firstname: 'deb',
+    //     lastname: 'lee',
+    //     email
+    // })
+  }
+  res.render("dashboard", {
+    title: "Dashboard",
     isAuthenticated: req.oidc.isAuthenticated(),
     user: req.oidc.user,
- });
+  });
 });
 
 module.exports = router;
